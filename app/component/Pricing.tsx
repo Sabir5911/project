bcff01c5618c88eb0209d1b5a6d20f458affc007
Sabir5911 <115262495+Sabir5911@users.noreply.github.com>
2023-01-23 03:11:29 +0500

    final

--- app/component/Pricing.tsx
+++ app/component/Pricing.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { icon } from '../icons/icon'
 
 
-export const ListItems=(props:StackProps)=>{
+export const Listitem=(props:StackProps)=>{
 
    const {children,...rest}=props
    return<>
@@ -17,10 +17,10 @@ export const ListItems=(props:StackProps)=>{
 
 export  function Pricing() {
   return (
-   <Box mx='6'>
-        <Box  maxW={'994px'} margin='auto' mt={"-340"} borderRadius='8px' overflow={'hidden'}   boxShadow="0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04);">
+   <Box mx='2'>
+       <Box  maxW={'994px'} margin='auto' mt={"-200px"} borderRadius='8px' overflow={'hidden'}   boxShadow="0px 20px 25px -5px rgba(0, 0, 0, 0.1), 0px 10px 10px -5px rgba(0, 0, 0, 0.04);">
      <Flex  direction={['column',"column",'row']}>
-     <Box as='section' borderTopRightRadius='0px' bg='#F0EAFB' p='43px'  fontFamily='sans-serif' textAlign={'center'}  >
+     <Box   bg='#F0EAFB' p='43px'  textAlign={'center'}  >
 
         <Text fontWeight='750' fontSize='24px' >Premium PRO</Text>
 
@@ -28,7 +28,7 @@ export  function Pricing() {
 
         <Text fontSize={'22px'} fontWeight='=800' mt='2' >billed just once</Text>
 
-        <Button  borderColor={'#805AD5'} bg='#805AD5' borderRadius={'8px'} fontWeight={'700'} w='300px' height={'33px'} mt='6'> 
+        <Button  borderColor={'#805AD5'} bg='#805AD5' borderRadius={'8px'} fontWeight={'700'} w='300px' height={'39px'} mt='6'> 
         <Text 
         fontSize={'20px'}
         fontFamily='sans-serif'
@@ -36,25 +36,25 @@ export  function Pricing() {
       >Get Started</Text></Button>
       
       </Box>
-
-<Box p="40px" fontSize="30px" bg="white"   >
-
-<Text  textAlign={'left'}>Access these features when you get this pricing package for your business.</Text>
-<Stack as="ul" spacing="20" pt="20" fontSize={'17px'} fontFamily={'sans-serif'} fontWeight='200' pl='unset'>
-              <ListItems>International calling and messaging API</ListItems>
-              <ListItems>Additional phone numbers</ListItems>
-              <ListItems>Automated messages via Zapier</ListItems>
-              <ListItems>24/7 support and consulting</ListItems>
-              sasaas
-            </Stack>
-
-</Box>
-
+      <Box
+                    maxW="994px" height={['425px', '350px', '300px']}
+                    p='60px' fontSize="16px" bg="white" >
+                    <Text>
+                        Access these features when you get this pricing package for your business.
+                    </Text>
+                    <Stack as="ul" spacing='20px' pt='24px'>
+                        <Listitem>International calling and messaging API</Listitem>
+                        <Listitem>Additional phone numbers</Listitem>
+                        <Listitem>Automated messages via Zapier</Listitem>
+                        <Listitem>24/7 support and consulting</Listitem>
+                    </Stack>
+                </Box>
 
      </Flex>
 
-          </Box>
+           </Box>
 
-          </Box>
+         //  </Box>
+         
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 import {Header} from '../app/component/Header'
 import { Box } from '@chakra-ui/react'
-import { Pricing } from './component/Pricing'
+import  {Pricing}  from './component/Pricing'
 import { Features } from './component/Features'
 export default function page() {
   return (
    <>  
-   <Box textAlign={'center'}>
+   
    <Header />
       <Pricing />
       <Features />
-   </Box>
+  
     
    </>
 

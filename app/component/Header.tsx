e37d4e2cba29980a0dedee482c026859ff7b4462
Sabir5911 <115262495+Sabir5911@users.noreply.github.com>
2023-01-23 03:15:40 +0500

    f_

--- app/component/Header.tsx
+++ app/component/Header.tsx
@@ -6,12 +6,12 @@ import React from 'react'
 import {Text,Box,Heading} from '@chakra-ui/react'
 export  function Header() {
   return (
-    <Box pb='112px'>
+   
   <Box  fontFamily='sans-serif' color='#F7FAFC' bgColor={'#6B46C1'} textAlign={["left","left",'center']}px='32px' pt='85px' pb='240px' margin={'-8px'}
   >
-         <Heading fontWeight={'extrabold'} fontSize={['3xl','3xl', '45px' ]} >Simple pricing for your business</Heading>
-         <Text fontWeight={'medium'} fontSize={['lg','lg','20px']}>Plans that are carefully crafted to suit your business.</Text>
+         <Heading fontWeight={'extrabold'} fontSize={['3xl','3xl', '5xl' ]} >Simple pricing for your business</Heading>
+         <Text fontWeight={'medium'} fontSize={['lg','lg','2xl']} pt='4'>Plans that are carefully crafted to suit your business.</Text>
 </Box>
-</Box>
+
   )
 }
